469ad77d5da6c8c55364f5211657de389e88d8b7
Vitoria Pacheco <vitoriac.pac@gmail.com>
2021-06-23 22:14:03 -0300

    Aula 3 - Depoimentos, Contato e rolamento

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 // Procurar seletor e adicionar na constante
 //Abre e fecha x r hamburguer
-const nav = document.querySelector('#header nav') 
+const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 // estrutura de repetição
-for(const element of toggle){
-  element.addEventListener('click', function() {
+for (const element of toggle) {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
@@ -15,22 +15,51 @@ for(const element of toggle){
 //Esconde quando clica em um item do menu
 const links = document.querySelectorAll('nav ul li a')
 //estrutura de repetição
-for(const link of links){
+for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
 //Mudar o header quanda der scroll
-const header = document.querySelector("#header")
+const header = document.querySelector('#header')
 const navHeigth = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-  if(window.scrollY >= navHeigth) {
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeigth) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
-  } else{
+  } else {
     //menor que a altura do header
     header.classList.remove('scroll')
   }
 })
+
+// Testimonials Swiper
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// ScrollReveal : Mostrar elementos no scroll
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
