9db3aa7cbdc830baeca64523e24f4ebea82671ad
Vitoria Pacheco <vitoriac.pac@gmail.com>
2021-06-23 23:38:49 -0300

    Aula 3 - Footer, Botão Topo

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-<h1>Projeto de Site responsivo criado durante as aulas da NLW Origin</h1>
+<h2>Projeto de Site responsivo criado durante as aulas da NLW Origin</h2>
 <p>
   Aprendendo conceitos de mobile first.
   Estrutura HTML, estrutura de cascata do CSS, lógica e scripts em JavaScript.
+
+Projeto no Figma.
+
+  <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQxsPYiGzNvGWPDU0XTQrLc%2FOrigin-Six-vitoriapac%3Fnode-id%3D0%253A1" allowfullscreen></iframe>
+
 </p>
--- main.js
+++ main.js
@@ -55,11 +55,23 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
   `
-  #home .image, #home .text
+  #home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+//Botão voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top')
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
