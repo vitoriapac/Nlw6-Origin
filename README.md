93b5857d654935a2f1a715ec18bb3b4af4451b5a
Vitoria Pacheco <vitoriac.pac@gmail.com>
2021-06-27 20:01:56 -0300

    Aula 5 - Media Querie

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 Projeto no Figma.
 
-  <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQxsPYiGzNvGWPDU0XTQrLc%2FOrigin-Six-vitoriapac%3Fnode-id%3D0%253A1" allowfullscreen></iframe>
+<p src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQxsPYiGzNvGWPDU0XTQrLc%2FOrigin-Six-vitoriapac%3Fnode-id%3D0%253A1"> Origin Six - Figma Project</p>
+
+Projeto criado primeiro para mobile e depois adaptado para outros formatos.
 
 </p>
--- main.js
+++ main.js
@@ -1,51 +1,53 @@
-// DOM Document Object Model
-
-// Procurar seletor e adicionar na constante
-//Abre e fecha x r hamburguer
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-// estrutura de repetição
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 
-//Esconde quando clica em um item do menu
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
-//estrutura de repetição
+
 for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
-//Mudar o header quanda der scroll
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
-const navHeigth = header.offsetHeight
+const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeigth) {
-    //scroll é maior que a altura do header
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
-    //menor que a altura do header
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
-// Testimonials Swiper
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
-// ScrollReveal : Mostrar elementos no scroll
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
@@ -54,24 +56,56 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(
-  `
-  #home .image, #home .text,
+  `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials,
+  #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links,
   footer .brand, footer .social
   `,
   { interval: 100 }
 )
 
-//Botão voltar para o topo
+/* Botão voltar para o topo */
 const backToTopButton = document.querySelector('.back-to-top')
 
-window.addEventListener('scroll', function () {
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
